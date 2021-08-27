fe2e75ea3e191fb3cd7f6be975ffd28deab168a6
vFf0621 <victorkwan1206@yahoo.com>
2021-08-27 10:59:12 -0700

    Added Kalman filter

--- ppm.ino
+++ ppm.ino
@@ -1,26 +1,38 @@
 #define channumber 8 //How many channels have your radio???
 #define filter 10 // Glitch Filter
 int channel[channumber]; //read Channel values
+int estimation[channumber]; //read Channel values
 int lastReadChannel[channumber]; //Last  values readed
 int conta=0; //couter
+#include <SimpleKalmanFilter.h>
+SimpleKalmanFilter Thro(0.001, 0.001, 0.000001);
+SimpleKalmanFilter Yaw(0.001, 0.001, 0.000001);
+SimpleKalmanFilter Pitch(0.001, 0.001, 0.000001);
+SimpleKalmanFilter Roll(0.001, 0.001, 0.000001);
+
 
 
 void setup()
 {
   Serial.begin(2000000); //Serial Begin
   pinMode(4, INPUT); //Pin 4 as input
-  pinMode(13, OUTPUT); // Led pin 13
+
 }
 
 void loop()
 {
-    Serial.println(String(map(channel[0], 694, 1531, 1099, 1939)) + ' ' + String(map(channel[1], 694, 1531, 1099, 1939)) + ' ' + String(map(channel[2], 694, 1531, 1099, 1939)) + ' ' + String(map(channel[3], 694, 1531, 1099, 1939)));
+   float x1 = Roll.updateEstimate(float(map(channel[0], 694, 1531, 1099, 1939)));
+   float x2 = Pitch.updateEstimate(float(map(channel[1], 694, 1531, 1099, 1939)));
+   float x3 = Thro.updateEstimate(float(map(channel[2], 694, 1531, 1099, 1939)));
+   float x4 = Yaw.updateEstimate(float(map(channel[3], 694, 1531, 1099, 1939)));
+    Serial.println(channel[0]);
 
   if(pulseIn(4, HIGH) > 3000) //If pulse > 3000 useconds, continues
   {
     for(int i = 0; i <= channumber-1; i++) //Read the pulses of the channels
     {
  channel[i]=pulseIn(4, HIGH);
+ 
     }
     for(int i = 0; i <= channumber-1; i++) //Average the pulses     { 
     if((channel[i] > 2000) || (channel[i] <100))//If channel > max range, chage the value to the last pulse
@@ -29,8 +41,8 @@ void loop()
  }
  else
  {
- channel[i]=(lastReadChannel[i]+channel[i])/2; //Average the last pulse eith the current pulse
  conta++; //increment counter
+ 
  }
 
     }
@@ -43,14 +55,7 @@ void loop()
 
    lastReadChannel[i]=channel[i];
  }
- if(channel[4] > 1000) //If channel 5 is > than 500 turn on the led
- {
-   digitalWrite(13, HIGH);
- }
- else
- {
-   digitalWrite(13, LOW);//If not turn it off
- }
+
  conta=0;//Restart counter.
     }
   } 
